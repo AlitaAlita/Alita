0270729d711751ffc959694782a95213afc7c284
RenJian <23418702@qq.com>
2020-02-14 22:07:51 +0800

    创建实体类，根据实体类创建数据库中的表

---
+++ Blog/src/main/java/com/alita/po/Comment.java
@@ -0,0 +1,116 @@
+package com.alita.po;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="t_comment")
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long    id;
+    private String nickName;
+    private String email;
+    private String  content;
+    private String  avatar;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @ManyToOne
+    private Blog blog;
+
+    @OneToMany(mappedBy = "parentComment")
+    private List<Comment> replyComments = new ArrayList<>();
+
+    @ManyToOne
+    private Comment parentComment;
+
+    public Comment() {
+    }
+
+    public List<Comment> getReplyComments() {
+        return replyComments;
+    }
+
+    public void setReplyComments(List<Comment> replyComments) {
+        this.replyComments = replyComments;
+    }
+
+    public Comment getParentComment() {
+        return parentComment;
+    }
+
+    public void setParentComment(Comment parentComment) {
+        this.parentComment = parentComment;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                ", email='" + email + '\'' +
+                ", content='" + content + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
