e9cdca9820ac67c8233dbff51b95066bae84185a
RenJian <23418702@qq.com>
2020-02-17 20:06:29 +0800

    分类管理功能优化部分BUG

--- Blog/src/main/java/com/alita/web/admin/TypeController.java
+++ Blog/src/main/java/com/alita/web/admin/TypeController.java
@@ -8,9 +8,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -27,20 +32,56 @@ public class TypeController {
     }
 
     @GetMapping("/types/input")
-    public String input(){
-
+    public String input(Model model){
+        model.addAttribute("type",new Type());
         return "admin/type-input";
     }
 
-    @PostMapping("/save")
-    public String save(Type type){
+    @PostMapping("/types")
+    public String save(@Valid Type type, BindingResult bindingResult, RedirectAttributes attributes){
+        if(typeService.getTypeByName(type.getName()) !=null){
+            bindingResult.rejectValue("name","nameErrors","改分类已存在，请勿重复添加！");
+        }
+        if(bindingResult.hasErrors()){
+            return "/admin/type-input";
+        }
         Type t=typeService.saveType(type);
         if(t==null){
-
+            attributes.addFlashAttribute("message","分类添加失败！" );
         }else{
-
+            attributes.addFlashAttribute("message","分类添加成功！" );
         }
-        return "redirect:admin/types";
+        return "redirect:/admin/types";
+    }
+
+    @GetMapping("types/{id}/input")
+    public String editUI(@PathVariable Long id, Model model){
+        model.addAttribute("type",typeService.getType(id));
+        return "/admin/type-input";
+    }
+
+    @PostMapping("/save/{id}")
+    public String editType(@PathVariable  Long id,Type type,BindingResult bindingResult,RedirectAttributes attributes){
+        if(typeService.getTypeByName(type.getName()) !=null){
+            bindingResult.rejectValue("name","nameErrors","改分类已存在，请勿重复添加！");
+        }
+        if(bindingResult.hasErrors()){
+            return "/admin/type-input";
+        }
+        Type t=typeService.updateType(id,type);
+        if(t==null){
+            attributes.addFlashAttribute("message","分类更新失败！" );
+        }else{
+            attributes.addFlashAttribute("message","分类更新成功！" );
+        }
+        return "redirect:/admin/types";
+    }
+
+    @GetMapping("types/{id}/delete")
+    public String delete(@PathVariable Long id,RedirectAttributes attributes){
+        typeService.deleteType(id);
+        attributes.addFlashAttribute("message","分类更新成功！" );
+        return "redirect:/admin/types";
     }
 
 
