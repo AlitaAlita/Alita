c634397de5e65cfa67d446f5d46ba74666c22163
Alita Alice <3441425824@qq.com>
2020-02-26 20:03:47 +0800

    add blog comments

---
+++ Blog/src/main/java/com/alita/web/CommentController.java
@@ -0,0 +1,43 @@
+package com.alita.web;
+
+import com.alita.po.Comment;
+import com.alita.service.BlogService;
+import com.alita.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @Author: Alita 1650810671@qq.com
+ * @Description:
+ * @Date: Created in 16:11 2020/2/26
+ * @Modified By:
+ */
+@Controller
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private BlogService blogService;
+
+    @Value(("${comment.avatar}"))
+    private String avatar;
+    @GetMapping("/comments/{blogId}")
+    public String comments(@PathVariable  Long blogId, Model model)
+    {
+        model.addAttribute("comments",commentService.listCommentByBlogId(blogId));
+        return "blog::commentList";
+    }
+@PostMapping("/comments")
+    public String post(Comment comment) {
+    Long blogId = comment.getBlog().getId();
+    comment.setBlog(blogService.getBlog(blogId));
+    comment.setAvatar(avatar);
+    commentService.saveComment(comment);
+        return "redirect:/comments/"+blogId;
+    }
+}
