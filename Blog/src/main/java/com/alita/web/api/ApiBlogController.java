65d746de726244de072e74d9dce56fc66c7689ad
RenJian <23418702@qq.com>
2020-03-09 15:59:28 +0800

    最新博客显示修复

--- Blog/src/main/java/com/alita/web/api/ApiBlogController.java
+++ Blog/src/main/java/com/alita/web/api/ApiBlogController.java
@@ -60,56 +60,10 @@ public class ApiBlogController {
         return new JsonResult(200,"success",blogs);
     }
 
-    @PostMapping("/blogs/search")
-    public String search(@PageableDefault(size = 5, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
-                         BlogQuery blog, Model model) {
-        model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return "admin/blogs :: blogList";
-    }
-
-    @GetMapping("/blogs/input")
-    public String input(Model model) {
-        setTypeAndTag(model);
-        model.addAttribute("blog", new Blog());
-        return INPUT;
-    }
-
-
-    private void setTypeAndTag(Model model) {
-        model.addAttribute("types", typeService.listType());
-        model.addAttribute("tags", tagService.listTag());
-    }
-
-    @GetMapping("/blogs/{id}/input")
-    public String editInput(@PathVariable  Long id, Model model) {
-        setTypeAndTag(model);
-        Blog blog = blogService.getBlog(id);
-        blog.init();
-        model.addAttribute("blog",blog);
-        return INPUT;
-    }
-
-    @PostMapping("/blogs")
-    public String post(Blog blog , RedirectAttributes attributes, HttpSession session)
-    {
-        blog.setUser((User) session.getAttribute("user"));
-        blog.setType(typeService.getType(blog.getType().getId()));
-        blog.setTags(tagService.listTag(blog.getTagIds()));
-       Blog b= blogService.saveBlog(blog);
-        if(b==null){
-            attributes.addFlashAttribute("message","操作失败！" );
-        }else{
-            attributes.addFlashAttribute("message","操作成功！" );
-        }
-        return REDIRECT_LIST;
-
-    }
-
-    @GetMapping("/blogs/{id}/delete")
-    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
-        blogService.deleteBlog(id);
-        attributes.addFlashAttribute("message", "删除成功");
-        return REDIRECT_LIST;
+    @GetMapping("/blog/{id}")
+    public JsonResult findBlogById(@PathVariable Long id) {
+        Blog blog = blogService.getAndConvert(id);
+        return new JsonResult(200,"success",blog);
     }
 
 }
